6638f99e3fec9ad9c7e514ed09337733014993eb
BernardZhao <bernard.zhao.us@gmail.com>
2019-05-31 15:46:32 -0700

    Turned output to logging

--- argparams.py
+++ argparams.py
@@ -2,6 +2,7 @@ import argparse
 import util
 import interval
 import sorting
+import logging
 
 
 def read_interval_function(int_function):
@@ -16,8 +17,8 @@ def read_interval_function(int_function):
             "none": interval.none
         }[int_function]
     except KeyError:
-        print(
-            "[WARNING] Invalid interval function specified, defaulting to 'threshold'.")
+        logging.warning(
+            "Invalid interval function specified, defaulting to 'threshold'.")
         return interval.threshold
 
 
@@ -31,33 +32,73 @@ def read_sorting_function(sorting_function):
             "saturation": sorting.saturation
         }[sorting_function]
     except KeyError:
-        print("[WARNING] Invalid sorting function specified, defaulting to 'lightness'.")
+        logging.warning("Invalid sorting function specified, defaulting to 'lightness'.")
         return sorting.lightness
 
 
 def parse_args():
     p = argparse.ArgumentParser(description="pixel mangle an image")
     p.add_argument("image", help="input image file")
-    p.add_argument("-o", "--output", help="output image file, defaults to a randomly generated string",
-                   default=util.id_generator() + ".png")
-    p.add_argument("-i", "--int_function",
-                   help="random, threshold, edges, waves, file, file-edges, none", default="threshold")
-    p.add_argument("-f", "--int_file",
-                   help="Image used for defining intervals", default="in.png")
-    p.add_argument("-t", "--threshold", type=float,
-                   help="Pixels darker than this are not sorted, between 0 and 1", default=0.25)
-    p.add_argument("-u", "--upper_threshold", type=float,
-                   help="Pixels darker than this are not sorted, between 0 and 1", default=0.8)
-    p.add_argument("-c", "--clength", type=int,
-                   help="Characteristic length of random intervals", default=50)
-    p.add_argument("-a", "--angle", type=float,
-                   help="Rotate the image by an angle (in degrees) before sorting", default=0)
-    p.add_argument("-r", "--randomness", type=float,
-                   help="What percentage of intervals are NOT sorted", default=0)
-    p.add_argument("-s", "--sorting_function",
-                   help="lightness, intensity, hue, saturation, minimum", default="lightness")
+    p.add_argument(
+        "-o", "--output",
+        help="output image file, defaults to a randomly generated string",
+        default=util.id_generator() + ".png"
+    )
+    p.add_argument(
+        "-i", "--int_function",
+        help="random, threshold, edges, waves, file, file-edges, none",
+        default="threshold"
+    )
+    p.add_argument(
+        "-f", "--int_file",
+        help="Image used for defining intervals",
+        default="in.png"
+    )
+    p.add_argument(
+        "-t", "--threshold", type=float,
+        help="Pixels darker than this are not sorted, between 0 and 1",
+        default=0.25
+    )
+    p.add_argument(
+        "-u", "--upper_threshold", type=float,
+        help="Pixels darker than this are not sorted, between 0 and 1",
+        default=0.8
+    )
+    p.add_argument(
+        "-c", "--clength", type=int,
+        help="Characteristic length of random intervals",
+        default=50
+    )
+    p.add_argument(
+        "-a", "--angle", type=float,
+        help="Rotate the image by an angle (in degrees) before sorting",
+        default=0
+    )
+    p.add_argument(
+        "-r", "--randomness", type=float,
+        help="What percentage of intervals are NOT sorted",
+        default=0
+    )
+    p.add_argument(
+        "-s", "--sorting_function",
+        help="lightness, intensity, hue, saturation, minimum",
+        default="lightness"
+    )
+    p.add_argument(
+        '-d', '--debug',
+        help="Print all debug statements",
+        action="store_const", dest="log_level", const=logging.DEBUG,
+        default=logging.WARNING,
+    )
+    p.add_argument(
+        '-v', '--verbose',
+        help="Print more info",
+        action="store_const", dest="log_level", const=logging.INFO,
+    )
     __args = p.parse_args()
 
+    logging.basicConfig(format="%(name)s: %(levelname)s - %(message)s", level=__args.log_level)
+    
     return {
         "image_input_path": __args.image,
         "output_image_path": __args.output,
@@ -74,14 +115,14 @@ def parse_args():
 
 def verify_args(args):
 
-    print("Interval function: ", args["interval_function"])
+    logging.info(f"Interval function: {args['interval_function']}")
     if args["interval_function"] in ["threshold", "edges", "file-edges"]:
-        print("Lower threshold: ", args["bottom_threshold"])
+        logging.info(f"Lower threshold: {args['bottom_threshold']}")
     if args["interval_function"] == "threshold":
-        print("Upper threshold: ", args["upper_threshold"])
+        logging.info(f"Upper threshold: {args['upper_threshold']}")
     if args["interval_function"] in ["random", "waves"]:
-        print("Characteristic length: ", args["clength"])
-    print("Randomness: ", args["randomness"], "%")
+        logging.info(f"Characteristic length: {args['clength']}")
+    logging.info(f"Randomness: {args['randomness']}%")
 
     args["interval_function"] = read_interval_function(
         args["interval_function"])
--- pixelsort.py
+++ pixelsort.py
@@ -5,54 +5,55 @@ except ImportError:
 from sorter import sort_image
 from argparams import parse_args, verify_args
 import util
+import logging
 
 
 def main(args):
     verify_args(args)
 
-    print("Opening image...")
+    logging.debug("Opening image...")
     input_img = Image.open(args["image_input_path"])
 
-    print("Converting to RGBA...")
+    logging.debug("Converting to RGBA...")
     input_img.convert('RGBA')
 
-    print("Rotating image...")
+    logging.debug("Rotating image...")
     input_img = input_img.rotate(args["angle"], expand=True)
 
-    print("Getting data...")
+    logging.debug("Getting data...")
     data = input_img.load()
 
-    print("Getting pixels...")
+    logging.debug("Getting pixels...")
     pixels = []
     for y in range(input_img.size[1]):
         pixels.append([])
         for x in range(input_img.size[0]):
             pixels[y].append(data[x, y])
 
-    print("Determining intervals...")
+    logging.debug("Determining intervals...")
     intervals = args["interval_function"](pixels, args)
 
-    print("Sorting pixels...")
+    logging.debug("Sorting pixels...")
     sorted_pixels = sort_image(pixels, intervals, args["randomness"], args["sorting_function"])
 
-    print("Placing pixels in output image...")
+    logging.debug("Placing pixels in output image...")
     output_img = Image.new('RGBA', input_img.size)
     for y in range(output_img.size[1]):
         for x in range(output_img.size[0]):
             output_img.putpixel((x, y), sorted_pixels[y][x])
 
     if args["angle"] is not 0:
-        print("Rotating output image back to original orientation...")
+        logging.debug("Rotating output image back to original orientation...")
         output_img = output_img.rotate(-args["angle"], expand=True)
 
-        print("Crop image to apropriate size...")
+        logging.debug("Crop image to apropriate size...")
         output_img = util.crop_to(
             output_img, Image.open(args["image_input_path"]))
 
-    print("Saving image...")
+    logging.debug("Saving image...")
     output_img.save(args["output_image_path"])
 
-    print("Done!", args["output_image_path"])
+    print(f"Done! {args['output_image_path']}")
 
 
 if __name__ == "__main__":
